f536794e309773c6d27f4928e0ca4c47ae6a5688
Bence Kozma <bencekozma@Bences-MacBook-Pro.local>
2019-01-22 15:09:26 +0000

    Implement show functionality for questions with slug instead of id

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -54,7 +54,8 @@ class QuestionsController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        $question->increment('views');
+        return view('questions.show')->with('question',$question);
     }
 
     /**
@@ -78,7 +79,7 @@ class QuestionsController extends Controller
     public function update(AskQuestionRequest $request, Question $question)
     {
         $question->update($request->only('title','body'));
-        return redirect()->route('questions.index')->with('success','Your question has been updated');
+        return redirect('/questions')->with('success','Your question has been updated');
     }
 
     /**
