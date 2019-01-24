e9e062de839b923bcbd4fc7fb55433c213c819aa
Bence Kozma <bencekozma@Bences-MacBook-Pro.local>
2019-01-24 12:33:18 +0000

    implement update answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -17,23 +17,21 @@ class AnswersController extends Controller
         $question->answers()->create(['body'=>$request->body,'user_id'=>\Auth::id()]);
         return back()->with('success','Your answer has been submitted successfully');
     }
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update',$answer);
+        return view('answers.edit',compact('question','answer'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
-    }
+        $this->authorize('update',$answer);
+        $answer->update($request->validate([
+            'body'=> 'required',
+        ]));
 
+        return redirect()->route('questions.show',$question->slug)->with('success','Your answer has been updated');
+    }
     /**
      * Remove the specified resource from storage.
      *
