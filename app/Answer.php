d5d70b2437b022c1f6e59f4d2eefd53cc2d58dc8
Bence Kozma <bencekozma@Bences-MacBook-Pro.local>
2019-01-24 12:48:54 +0000

    Update best answer id on questions table when the answer removed using eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -21,6 +21,9 @@ class Answer extends Model
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
     }
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
     public static function boot(){
         parent::boot();
         static::created(function($answer){
@@ -28,7 +31,12 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
-    } 
+    }
 }
