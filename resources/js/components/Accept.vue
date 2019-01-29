f1e5f2e6bbe3cfd52f714b043a16b78bd97fc507
Bence Kozma <bencekozma@Bences-MacBook-Pro.local>
2019-01-29 16:46:20 +0000

    Fixing the accept answer button in Vue.js

--- resources/js/components/Accept.vue
+++ resources/js/components/Accept.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import EventBus from '../event-bus.js';
 export default {
     props: ['answer'],
     data(){
@@ -21,6 +22,11 @@ export default {
             isBest: this.answer.is_best,
             id: this.answer.id,
         }
+    },
+    created(){
+        EventBus.$on('accepted',id=>{
+            this.isBest = (id === this.id);
+        })
     },
     methods:{
         create(){
@@ -31,6 +37,7 @@ export default {
                     position: 'bottomLeft',
                 });
                 this.isBest = true;
+                EventBus.$emit('accepted',this.id);
             });
         }
     },
